c0879590d2b60eb5a100387b8d93f10eb2387970
1221209283 <1221209283@student.mmu.edu.my>
2024-07-07 17:25:04 +0800

    Formatting
    Signed-off-by: 1221209283 <1221209283@student.mmu.edu.my>

--- extra3/E3Q6_no_virt.cpp
+++ extra3/E3Q6_no_virt.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
 using namespace std;
 
-class Job
-{ protected:
-float salary;
+class Job{ 
+protected:
+  float salary;
 public:
-virtual void display ( float s )
-{ salary = s;
-cout<< "= Your salary is :: RM " << salary<< endl;
-}
-~Job()
-{ cout<< "Job class" << endl << endl; }
+  virtual void display ( float s ) {
+    salary = s;
+    cout<< "= Your salary is :: RM " << salary<< endl;
+  }
+  ~Job() {
+    cout<< "Job class" << endl << endl;
+  }
 };
-class Lecturer: public Job
-{ void display( float s )
-{ salary = s;
-cout<< "This is a Lecturer’s salary :" << salary << endl;
-}
-~Lecturer()
-{ static int num=1;
-cout << "Lecturer " << num << " salary calculation done" <<
-endl ;
-num++;
-}
+
+class Lecturer: public Job{
+  void display( float s ) {
+    salary = s;
+    cout<< "This is a Lecturer’s salary :" << salary << endl;
+  }
+  ~Lecturer() {
+    static int num=1;
+    cout << "Lecturer " << num << " salary calculation done" <<endl;
+    num++;
+  }
 };
-int main()
-{ Job j;
-Job *p = new Lecturer;
-p->display(1500);
-delete p;
-p = new Lecturer;
-p->display(2590);
-delete p;
-p = &j;
-p->display(150);
-return 0;
+
+int main(){
+  Job j;
+
+  Job *p = new Lecturer;
+  p->display(1500);
+  delete p;
+
+  p = new Lecturer;
+  p->display(2590);
+  delete p;
+
+  p = &j;
+  p->display(150);
+
+  return 0;
 }
